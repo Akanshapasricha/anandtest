7c84d48c7dcc7913551ed4179d8afac4b30055c0
anand.kumar <anand.kumar@Source-6625-pc.sourcesoftsolutions.com>
2019-07-04 18:01:27 +0530

    test

---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class Demo 
+{
+  public WebDriver driver;
+  
+  @Test
+  public void brower_opening()
+  {
+	  System.setProperty("webdriver.chrome.driver","D:\\WebFramework\\ProjectTest\\exefiles\\chromedriver.exe");
+	  driver= new ChromeDriver();
+	  driver.get("http://148.76.75.203/united_missions/");
+	  
+  }
+	
+	
+}
